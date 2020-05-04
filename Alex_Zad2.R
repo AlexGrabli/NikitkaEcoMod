27c56a733087d57633d062f88758141899f69cbd
Никита Александров <punxlov@gmail.com>
2020-05-04 21:10:18 +0300

    Фрейм приведен в более адекватный вид (но это не точно), отфильрованы данные по дате и времени суток

--- Alex_Zad2.R
+++ Alex_Zad2.R
@@ -1,27 +1,45 @@
-data = read_csv("eddypro.csv", skip = 1, na = "-9999")
 #Александров Н.А. 125 ПАЭ 
 #Задание 2
-#создайте модель множественной линейной регрессии дневных потоков паров воды за весенний период 2013 года по данным измерений методом турбулентной пульсации
+#создайте модель множественной линейной регрессии дневных потоков паров воды за весенний период 2013 года 
+#по данным измерений методом турбулентной пульсации
 
-library(readr)
-tbl = read_csv("eddypro.csv", skip = 1, na =c("","NA","-9999","-9999.0"),  comment=c("["))
-tbl
-tbl=tbl[-1,]
-tbl
-glimpse(tbl)
-tbl = select(tbl, -(roll))
-tbl
-tbl = tbl %>% mutate_if(is.character, factor)
-tbl
-names(tbl) =  str_replace_all(names(tbl), "[!]","_emph_")%>%
-  tbl
-glimpse(tbl)
-sapply(tbl,is.numeric)
-tbl_numeric = tbl[,sapply(tbl,is.numeric) ]
-tbl_non_numeric = tbl[,!sapply(tbl,is.numeric) ]
-cor_td = cor(tbl_numeric)
-cor_td
-cor_td = cor(drop_na(tbl_numeric))
-cor_td
-cor_td = cor(drop_na(tbl_numeric)) %>% as.data.frame %>% select(h2o_flux)
-vars = row.names(cor_td)[cor_td$h2o_flux^2 > .1] %>% na.exclude
+#Подключаем пакеты
+library("tidyverse") 
+library("stringr")    
+library("dplyr")      
+library("ggplot2")  
+library("tibble")
+library("readr") 
+library("rnoaa")
+library("lubridate")
+
+#Считываем данные из файла; Пропускаем первую строчку, заменяем текстовые NA,
+#заменяем все не числовые значения на NA, пропускаем строчки с "["
+data = read_csv("eddypro.csv", skip = 1, na =c("","NA","-9999","-9999.0"),  comment=c("["))
+#удаляем ненужный пустой столбец
+data = select(data, -(roll))
+#Преобразуем строковые значения в факторные
+data = data %>% mutate_if(is.character, factor)
+#Заменяем символы в названии столбцов на допустимые для переменных названия
+names(data) = names(data) %>% 
+  str_replace_all("[!]", "_exclam_") %>% 
+  str_replace_all("[?]", "_quest_") %>% 
+  str_replace_all("[*]", "_star_") %>% 
+  str_replace_all("[+]", "_plus_") %>%
+  str_replace_all("[-]", "_minus_") %>%
+  str_replace_all("[@]", "_at_") %>%
+  str_replace_all("[$]", "_dollar_") %>%
+  str_replace_all("[#]", "_hash_") %>%
+  str_replace_all("[/]", "_slash_") %>%
+  str_replace_all("[%]", "__pecent_") %>%
+  str_replace_all("[&]", "_amp_") %>%
+  str_replace_all("[\\^]", "_power_") %>%
+  str_replace_all("[()]", "_")
+#Превращаем столбцы таблицы в виде векторов для проверки
+glimpse(data)
+#Убераем NA
+data = drop_na(data)
+#Отфильтруем данные для летнего периода (1 марта  - 60 день, 31 мая - 151 день)
+data = filter(data, DOY >= 60 & DOY <= 151)
+#Отфильтруем данные для дневного периода
+data = filter(data, daytime==TRUE)
